be9c2e8d4a1911f206e3133c1c563187fb20dfe6
Danila Romanov <danila.romanof@gmail.com>
2022-11-01 17:23:42 +0200

    Minor update

--- src/main/java/hello/HelloServlet.java
+++ src/main/java/hello/HelloServlet.java
@@ -1,13 +1,7 @@
 package hello;
 
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
 
 @RestController
 public class HelloServlet {
