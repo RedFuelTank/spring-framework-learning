2bf06c7edcee2a16af40b8e44015dd29e20a038e
Danila Romanov <danila.romanof@gmail.com>
2022-11-20 16:58:56 +0200

    minor update

--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
